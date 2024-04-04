5c10f70eb89bfe2a3831c29064fda01ac4740175
Benji Uribe Ramos <benji@staffdigital.pe>
2024-04-04 11:55:21 -0500

    dotenv

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 #### npm run dev
 
 El proyecto contiene endpoints para hacer un crud de una tabla en mysql, para los endpoints se esta usando express.
+
+CREAR UNA BASE DE DATOS mysql Y IMPORTAR LOS SCRIPT SQL sakila-data.sql y sakila-schema.sql
--- src/connection/index.js
+++ src/connection/index.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2/promise')
 
+const path = require('path')
+require('dotenv').config({ path: path.resolve('_dirname', '../.env') })
+
 const connection = mysql.createPool({
-    host: "localhost",
-    port: "3306",
-    database: "sakila",
-    user: "root",
-    password: "98738534"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
 });
 
 module.exports = connection;
